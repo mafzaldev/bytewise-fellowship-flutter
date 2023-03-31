b52140d9bf6b307ef7fa6f88b3a645cd7388134f
Afzal <afzalashraf814@gmail.com>
2023-04-01 00:32:03 +0500

    initialized Week03

---
+++ Week-03/README.md
@@ -0,0 +1,3 @@
+# Week 03 (Firebase)
+
+In this week, I learnt, how to work with Firebase in Flutter.
---
+++ Week-03/firebase_tut/android/app/src/main/kotlin/com/example/firebase_tut/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebase_tut
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
